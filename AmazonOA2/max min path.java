379b9c869b4d3b379a287e9d3330865327832e96
Bohuai Jiang <bohuaijiang.1021@tamu.edu>
2019-02-22 20:54:26 -0600

    Update max min path.java

--- AmazonOA2/max min path.java
+++ AmazonOA2/max min path.java
@@ -37,3 +37,29 @@ public class Solution {
     }
  
 }
+
+
+// method2 : DFS
+ 
+public class MaximumMinimumPath {
+   private int min, max, row, col;
+   public int maxMinPath(int[][] matrix) {
+       row = matrix.length;
+       col = matrix[0].length;
+       min = Integer.MAX_VALUE;
+       max = Integer.MIN_VALUE;
+       dfsHelper(matrix, min, 0, 0);
+       return max; 
+    }
+  public void dfsHelper(int[][] matrix, int min, int i, int j ){
+    if (i >= row || j >= col) return;
+    if (i == row - 1 && j == col - 1) {
+        min = Math.min(min, matrix[i][j]);
+        max = Math.max(max, min);
+        return;
+    }
+    min = Math.min(min, matrix[i][j]);
+    dfsHelper(matrix, min, i, j + 1);
+    dfsHelper(matrix, min, i + 1, j); 
+  }
+}
