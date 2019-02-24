4574cb391a93cd1a59bb357590854c0dd566b639
Bohuai Jiang <bohuaijiang.1021@tamu.edu>
2019-02-24 11:22:41 -0600

    Update maze II.java

--- AmazonOA2/maze II.java
+++ AmazonOA2/maze II.java
@@ -1,62 +1,59 @@
-public class Solution {
-    /**
-     * @param maze: the maze
-     * @param start: the start
-     * @param destination: the destination
-     * @return: the shortest distance for the ball to stop at the destination
-     */
+class Solution {
+    int[] dx = new int[]{1,-1,0,0};
+    int[] dy = new int[]{0,0,1,-1};
+    int m = 0, n = 0;
     class Point {
         int x;
         int y;
-        int steps;
-        public Point(int x, int y, int steps) {
+        int step;
+        public Point(int x, int y, int step) {
             this.x = x;
             this.y = y;
-            this.steps = steps;
+            this.step = step;
         }
     }
-    public int[] delataX = {0, 0, 1, -1};
-    public int[] delataY = {1, -1, 0, 0};
-  
-  // When the ball stops, it could choose the next direction.
-  // BFS + Memory
     public int shortestDistance(int[][] maze, int[] start, int[] destination) {
-        if (maze == null || maze.length == 0 || maze[0].length == 0) return -1;
-        if (maze[start[0]][start[1]] == 1 ||
-            maze[destination[0]][destination[1]] == 1) return -1;
-        
-        
-        Queue<Point> queue = new LinkedList<>();
-        int[][] res = new int[maze.length][maze[0].length];
-        for (int i = 0; i < res.length; i++) {
+        if (maze == null || maze.length == 0 || maze[0] == null) {
+            return -1;
+        }
+        if (maze[start[0]][start[1]] == 1 || maze[destination[0]][destination[1]] == 1) {
+            return 0;
+        }
+        m = maze.length;
+        n = maze[0].length;
+        int[][] res = new int[m][n];
+        for (int i = 0; i < m; i++) {
             Arrays.fill(res[i], -1);
         }
-        queue.offer(new Point(start[0], start[1], 0));
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i = 0; i < size; i++) {
-                Point cur = queue.poll();
-                if (res[cur.x][cur.y] != -1 && cur.steps >= res[cur.x][cur.y]) {
-                  continue;
+        Queue<Point> q = new LinkedList<Point>();
+        q.add(new Point(start[0], start[1], 0));
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Point head = q.poll();
+                int x = head.x;
+                int y = head.y;
+                if (res[x][y] != -1 && head.step >= res[x][y]) {
+                    continue;
                 }
-                res[cur.x][cur.y] = cur.steps;
+                res[x][y] = head.step;
                 for (int j = 0; j < 4; j++) {
-                    int x = cur.x, y = cur.y, steps = cur.steps;
-                    while (x >= 0 && x < maze.length &&
-                           y >= 0 && y < maze[0].length &&
-                           maze[x][y] == 0) {
-                        x += delataX[j];
-                        y += delataY[j];
-                        steps++;
+                    int step = head.step;
+                    int dirX = x + dx[j];
+                    int dirY = y + dy[j];
+                    step++;  // 因为根据上面两行，已经走了一步；
+                    while (dirX >= 0 && dirX < m && dirY >= 0 && dirY < n && maze[dirX][dirY] == 0) {
+                        dirX += dx[j];
+                        dirY += dy[j];
+                        step++;
                     }
-                    x -= delataX[j];
-                    y -= delataY[j];
-                    steps--;
-                    queue.offer(new Point(x, y, steps));
+                    step--;
+                    dirX -= dx[j];
+                    dirY -= dy[j];
+                    q.add(new Point(dirX, dirY, step));
                 }
             }
         }
         return res[destination[0]][destination[1]];
-    } 
+    }
 }
